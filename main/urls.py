6d368479e80239a4f00b49b8aa561af1b6a415ac
Rogelio <rogercp1234@yahoo.com>
2020-05-07 18:40:36 -0700

    brought in login url for users

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
 from django.urls import include, path
+from django.conf.urls import url
 from . import api
 
 urlpatterns = [
     path('registration/', include('rest_auth.registration.urls')),
+    path('', include('rest_auth.urls')),
     url('init/', api.initialize),
-
 ]
